3fdaf155ca25624f509f2eecb313063bb400e49a
igor <85964276+igcardoso@users.noreply.github.com>
2023-03-10 09:39:41 -0300

    update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -128,7 +128,7 @@ function createImageMovie(movieImage, movieTitle) {
 
 function addMovieInList(movie) {
   const movieElement = document.createElement('li')
-  movieElement.classList.add('movie');
+  movieElement.classList.add('movie' + movie.title);
 
   movieElement.setAttribute('id', movie.id);
 
