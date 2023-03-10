9d13b78bb853cc1b6876912acca541c81b675321
igor <85964276+igcardoso@users.noreply.github.com>
2023-03-10 09:30:26 -0300

    update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -69,7 +69,7 @@ function setMainMovie(movie) {
       document.querySelector("#main").style.display = "none";
     }, 2000);
   });
-  title.innerHTML = movie.name;
+  title.innerHTML = movie.title;
   description.innerHTML = movie.overview;
   rating.innerHTML = movie.vote_average;
   info.innerHTML = movie.release + ' - ' + movie.genre + ' - Filme';
