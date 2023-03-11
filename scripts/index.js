a3f03730fe9867cc20c3795a5ea3c299c99fff0a
igor <85964276+igcardoso@users.noreply.github.com>
2023-03-11 09:47:02 -0300

    update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -70,10 +70,7 @@ function setMainMovie(movie) {
           document.querySelector(".orientation_video").pause();
           locationMovies.src = "";
           pageFilm.style.display = "none";
-          document.querySelector("#app").style.display = "flex";
-          document.querySelector("#header").style.display = "flex";
-          document.querySelector("#navigation").style.display = "block";
-          document.querySelector("#main").style.display = "flex";
+          select__player.style.display = "block";
         });
         locationMovies.src = /*'https://embedder.net/e/movie?imdb='*/ 'https://embedflix.net/filme/' + idFilm;
         pageFilm.style.display = "block";
