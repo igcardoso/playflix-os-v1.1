a5db0966b68e8bb5a373d0f9a92c756e97262230
igor <85964276+igcardoso@users.noreply.github.com>
2023-03-11 19:57:55 -0300

    update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -54,9 +54,9 @@ function setMainMovie(movie) {
       var orientation_alert = document.querySelector("#pageFilm  .orientation_alert");
       var marker_contention = document.querySelector("#pageFilm  .marker_contention");
 
-      // loading.style.left = "0";
+      loading.style.left = "0";
       setTimeout(function() {
-        // loading.style.left = "-100%";
+        loading.style.left = "-100%";
         marker_contention.addEventListener("click", ()=> {
           orientation_alert.style.transform = "translateY(-100%)";
           document.querySelector(".orientation_video").pause();
@@ -80,7 +80,7 @@ function setMainMovie(movie) {
         document.querySelector("#navigation").style.display = "none";
         document.querySelector("#main").style.display = "none";
         select__player.style.display = "none";
-      }, 2000);
+      }, 500);
     });
     
     // Segunda seleção de player
